fc519c105b3e55fd87dce59492bc4ce940358d04
奇葩の灵梦 <415551921@qq.com>
2022-01-07 23:52:03 +0800

    缓存会话

--- main.go
+++ main.go
@@ -50,10 +50,11 @@ func main() {
 
 	// 刷新好友列表，群列表
 	bot.RefreshList()
-
+	bot.SaveToken()
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	<-ch
+	bot.SaveToken()
 	bot.Stop()
 	db.Stop()
 }
